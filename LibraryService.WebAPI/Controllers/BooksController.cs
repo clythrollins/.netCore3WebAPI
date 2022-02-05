6255522f981dc2b8a1ffdacf9c6750892fff7ba3
okinih mim <49981227+clythrollins@users.noreply.github.com>
2022-02-05 23:27:34 +0800

    add files via upload
    web api tests with post, get and delete method, and http status

---
+++ LibraryService.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryService.WebAPI.Data;
+using LibraryService.WebAPI.Services;
+using System.Collections.Generic;
+
+namespace LibraryService.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksService _bookrepository;
+
+        public BooksController(IBooksService bookrepository)
+        {
+            this._bookrepository = bookrepository;
+        }
+
+        [HttpGet("{libraryId, ids}")]
+        public IActionResult GetBooks(int libraryId, int[] ids)
+        {
+            var getbooks = _bookrepository.Get(libraryId, ids);
+            if (getbooks == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(getbooks);
+        }
+
+        [HttpPost("{book}")]
+        public async Task<ActionResult<Book>> AddBooks([FromBody] Book book)
+        {
+            var newBook = await _bookrepository.Add(book);
+            return CreatedAtAction(nameof(GetBooks), new { Id = newBook.Id }, newBook);
+        }
+    }
+}
---
+++ LibraryService.WebAPI/Controllers/LibrariesController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryService.WebAPI.Data;
+using LibraryService.WebAPI.Services;
+using System;
+
+namespace LibraryService.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class LibrariesController : ControllerBase
+    {
+        private readonly ILibrariesService _librariesrepository;
+
+        public LibrariesController(ILibrariesService librariesrepository)
+        {
+            _librariesrepository = librariesrepository;
+        }
+
+        [HttpDelete("{library}")]
+        public async Task<ActionResult> Delete(int[] ids)
+        {
+            var DeleteLibrary = await _librariesrepository.Get(ids);
+            if (DeleteLibrary == null)
+            {
+                return NotFound();
+            }
+
+            await _librariesrepository.Delete(DeleteLibrary.FirstOrDefault());
+            return NoContent();
+        }
+    }
+}
